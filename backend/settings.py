0ebd24af2da6951112c754c4990db8c7028bb8a4
devendra-pandey <devendra.dpandey02@gmail.com>
2021-07-13 22:38:19 +0530

    demo video added

--- backend/settings.py
+++ backend/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 3,
+    'PAGE_SIZE': 4,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         # 'rest_framework.authentication.BasicAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
